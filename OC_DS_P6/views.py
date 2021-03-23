1a0f6b658cf00a7d3cd544e464209954fe53ac06
FKO78 <71488429+FKO78@users.noreply.github.com>
2021-03-23 22:02:28 +0100

    debug

--- OC_DS_P6/views.py
+++ OC_DS_P6/views.py
@@ -68,7 +68,7 @@ def tag_question():
         tfidf_full = hstack([tfidf_t, tfidf_b])
 
         #result = get_tags(label.classes_, model.predict(tfidf_full)[0])
-        result = str(sum(list(model.predict(tfidf_full)[0])))
+        result = len(model.predict(tfidf_full)[0])
 
     return dumps({'_In': {'Titre' : titre[:30] + '...',
                           'Detail' : corps[:30] + '...'},
