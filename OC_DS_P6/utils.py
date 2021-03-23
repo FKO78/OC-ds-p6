cc88fe0c07c95886c40da1897410a58ec0d28313
FKO78 <71488429+FKO78@users.noreply.github.com>
2021-03-23 13:36:23 +0100

    import pos_tag in views

--- OC_DS_P6/utils.py
+++ OC_DS_P6/utils.py
@@ -21,7 +21,7 @@ def get_wordnet_pos(word):
     Map POS tag to first character lemmatize() accepts
     """
 
-    tag = nltk.pos_tag([word])[0][1][0].upper()
+    tag = pos_tag([word])[0][1][0].upper()
     tag_dict = {"J": wordnet.ADJ,
                 "N": wordnet.NOUN,
                 "V": wordnet.VERB,
--- OC_DS_P6/views.py
+++ OC_DS_P6/views.py
@@ -8,6 +8,7 @@ from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.tokenize import RegexpTokenizer
 from scipy.sparse import hstack
 from .utils import *
+from nltk import pos_tag
 
 app = Flask(__name__)
 
